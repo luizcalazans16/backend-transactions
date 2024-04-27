7bec71c5ab10d955054a343acc8d986f8c49563a
Luiz Fernando Calazans <luiz.calazans@ifood.com.br>
2024-04-27 12:44:20 -0300

    Initial commit

---
+++ core-banking-service/src/main/kotlin/com/calazans/core/banking/service/CoreBankingServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.calazans.core.banking.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoreBankingServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoreBankingServiceApplication>(*args)
+}
---
+++ core-banking-service/src/test/kotlin/com/calazans/core/banking/service/CoreBankingServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.calazans.core.banking.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreBankingServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
